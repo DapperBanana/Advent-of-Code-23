b6e2b3ccfb2e42ffa43fe76dbc7cda9f7ed4e544
Austin L. Howard <austin.lane.howard@gmail.com>
2023-12-02 22:26:13 -0700

    Create Nov-29-23-Part1.py
    Day 1 & Challenge 1

---
+++ Nov-29-23-Part1.py
@@ -0,0 +1,24 @@
+# Open the text file for reading
+file_path = "C:\\Users\\Austin.Howard\\Downloads\\input.txt"
+try:
+    with open(file_path, "r") as file:
+        final_num = 0
+        # Loop over each line in the file
+        for line in file:
+            # Process each line here
+            first_digit = None
+            last_digit = None
+            for char in line:
+                if char.isdigit():
+                    if first_digit is None:
+                        first_digit = int(char)
+                        last_digit = int(char)
+                    else:
+                        last_digit = int(char)
+            combined_number = int(str(first_digit) + str(last_digit))
+            final_num += combined_number
+        print(final_num)
+except FileNotFoundError:
+    print(f"File not found: {file_path}")
+except Exception as e:
+    print(f"An error occurred: {str(e)}")
